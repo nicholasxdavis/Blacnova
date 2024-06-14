3ef6786ea5d4187ab791adaae1d96caa28c04dfd
blacnova <147455788+nicholasxdavis@users.noreply.github.com>
2024-06-14 01:58:02 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        this.textContent = '🌗';
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        this.textContent = '🌕';
+    }
+    updateLogo();
+});
+
+// Set the initial state based on the saved theme preference
+document.addEventListener('DOMContentLoaded', function() {
+    const savedTheme = localStorage.getItem('theme');
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+        themeToggleButton.textContent = savedTheme === 'dark' ? '🌕' : '🌗';
+    } else {
+        // Set default theme to light if no preference is saved
+        document.documentElement.setAttribute('data-theme', 'light');
+        themeToggleButton.textContent = '🌗';
+    }
+    updateLogo();
+});
+
+function updateLogo() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const logoImg = document.getElementById('logo-img');
+    logoImg.src = currentTheme === 'dark' ? 'img/white.png' : 'img/black.png';
+}
